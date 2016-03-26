4024af4d4a957bf2f68ac470eace68beda3b63b8
acessac <acessac@incomm.com>
2016-03-26 15:06:47 -0700

    set up events for Philosopher getting food.

--- HungryPhilosophers/HungryPhilosophers/Table.cs
+++ HungryPhilosophers/HungryPhilosophers/Table.cs
@@ -11,17 +11,38 @@ namespace HungryPhilosophers
     {
         private AutoResetEvent[] Chopsticks;
 
+        private ManualResetEvent FoodAvailable;
+
         FoodItem Dish;
         PhilosopherParty Party;
+
+        public void ServeTable(FoodItem dish)
+        {
+
+            //Maybe?
+            Dish = dish;
+            Dish.CanGetFood = FoodAvailable;
+            FoodAvailable.Set();
+
+        }
 
         public void SetTable(int numEaters)
         {
             var numChopsticks = numEaters > 1 ? numEaters : 2;
 
             Chopsticks = new AutoResetEvent[numChopsticks];
+            for (var i = 0; i < Chopsticks.Length - 1; i++)
+                Chopsticks[i] = new AutoResetEvent(false);
+
             Party.Philosophers = new Philosopher[numEaters];
 
-            EatFood += Feed;
+            for (var i = 0; i < Party.Length -1; i++)
+            {
+                Party.Philosophers[i].EatFromTable += EaterGetsFood;
+                Party.Philosophers[i].Satisfaction = 0;
+            }
+
+            
         }
 
         public void SeatEaters()
@@ -36,8 +57,8 @@ namespace HungryPhilosophers
         {
             Party.Philosophers[id] = new Philosopher(id)
             {
-                LeftChopstick = GetLeftChopstick(id),
-                RightChopstick = GetRightChopstick(id)
+                LeftChopstick = Chopsticks[GetLeftChopstick(id)],
+                RightChopstick = Chopsticks[GetRightChopstick(id)]
             };
         }
 
@@ -51,14 +72,22 @@ namespace HungryPhilosophers
             return eaterId == Party.Length - 1 ? 0 : eaterId + 1;
         }
 
-        void Feed(Philosopher eater)
+        public void EaterGetsFood(Philosopher eater, TakeBitEventArgs e)
         {
             
-        }
-        
-        public delegate void TakeBite(Philosopher eater);
+            FoodAvailable.WaitOne();
 
-        public event TakeBite EatFood;
+            
+
+            eater.HungerLevel --;
+            Dish.Bites --;
+
+
+
+        }
+
+
+        
     }
 
     internal class PhilosopherParty
@@ -69,16 +98,40 @@ namespace HungryPhilosophers
 
     public class Philosopher
     {
-        internal int RightChopstick { get; set; }
-        internal int LeftChopstick { get; set; }
-        public int HungerLevel { get; private set; }
+        public bool Isfull { get; private set; }
+        internal AutoResetEvent RightChopstick { get; set; }
+        internal AutoResetEvent LeftChopstick { get; set; }
+        public int Satisfaction { get; set; }
+        public int HungerLevel { get; set; }
 
         public Philosopher(int id)
         {
             Id = id;
         }
         internal int Id { get; private set; }
-        
+
+        public void EatFoodUntillFull()
+        {
+            Eat(new TakeBitEventArgs {});
+        }
+
+        public delegate void EatHandler(Philosopher foodItem, TakeBitEventArgs e);
+
+        public event EatHandler EatFromTable;
+
+        public void Eat(TakeBitEventArgs e)
+        {
+            LeftChopstick.WaitOne();
+            RightChopstick.WaitOne();
+            EatFromTable?.Invoke(this, e);
+            LeftChopstick.Set();
+            RightChopstick.Set();
+        }
+
+        protected virtual void OnEatFood(Philosopher eater, TakeBitEventArgs e)
+        {
+            
+        }
     }
 
     public class TakeBitEventArgs : EventArgs
@@ -88,7 +141,27 @@ namespace HungryPhilosophers
 
     public class FoodItem
     {
-        private int Bites { get; set; }
-        private int SatisfactionAmount { get; set; }
+        public int Bites { get; set; }
+        public int SatisfactionAmount { get; private set; }
+        public ManualResetEvent CanGetFood { get; set; }
+
+        public delegate void TakeBiteHandler(Philosopher eater, TakeBitEventArgs e);
+
+        public event TakeBiteHandler TakeBiteEvent;
+
+        public void TakeBite()
+        {
+            Bites --;
+
+            if (Bites == 0)
+            {
+                CanGetFood.Reset();
+            }
+        }
+
+        protected virtual void OnTakeBiteEvent(Philosopher eater, TakeBitEventArgs e)
+        {
+            TakeBiteEvent?.Invoke(eater, e);
+        }
     }
 }
